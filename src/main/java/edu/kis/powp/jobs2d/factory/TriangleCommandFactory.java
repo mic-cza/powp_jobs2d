3528b344fb2d2a54314ca02bae59cb13f6f00e89
mic-cza <czarneckimichal92@gmail.com>
2025-12-16 18:50:29 +0100

    addded TriangleCommandFactory

--- src/main/java/edu/kis/powp/jobs2d/factory/TriangleCommandFactory.java
+++ src/main/java/edu/kis/powp/jobs2d/factory/TriangleCommandFactory.java
@@ -7,18 +7,16 @@ import edu.kis.powp.jobs2d.command.SetPositionCommand;
 
 import java.awt.*;
 
-public class RectangleCommandFactory extends ShapeCommandFactory{
+public class TriangleCommandFactory extends ShapeCommandFactory{
     private final Point p1;
     private final Point p2;
     private final Point p3;
-    private final Point p4;
 
 
-    public RectangleCommandFactory(Point p1, Point p2, Point p3, Point p4) {
+    public TriangleCommandFactory(Point p1, Point p2, Point p3) {
         this.p1 = p1;
         this.p2 = p2;
         this.p3 = p3;
-        this.p4 = p4;
     }
 
     @Override
@@ -27,7 +25,6 @@ public class RectangleCommandFactory extends ShapeCommandFactory{
         command.addCommand(new SetPositionCommand(p1.x, p1.y));
         command.addCommand(new OperateToCommand(p2.x, p2.y));
         command.addCommand(new OperateToCommand(p3.x, p3.y));
-        command.addCommand(new OperateToCommand(p4.x, p4.y));
         command.addCommand(new OperateToCommand(p1.x, p1.y));
         return command;
     }
